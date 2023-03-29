36dffab75eb43852d35447427a021e4cee8a9cf3
ottojoash <louisjoshbricks@gmail.com>
2023-03-29 10:42:31 +0300

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { store } from "@/store";
+
+import { Layout } from "@/components/Layout";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
+
+export default MyApp;
